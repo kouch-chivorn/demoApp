a92a9b3bf05c56eef0f54816c9be7ba2bc9fabbe
kouch-chivorn <kouch.chivorn@framgia.com>
2015-07-30 17:52:17 +0700

    finish

--- app.js
+++ app.js
@@ -1,32 +1,65 @@
+angular.module("demoApp",['ui.router'])
+  .config(["$stateProvider", "$urlRouterProvider",
+    function($stateProvider, $urlRouterProvider){
+      $stateProvider
+        .state("home",{
+          url: "/home",
+          templateUrl: "/index.html",
+          controller: "BookController"
+        })
+        .state("cart",{
+          url: "/cart",
+          templateUrl: "/cart.html",
+          controller: "CartController"
+        });
+      $urlRouterProvider.otherwise("home");
+    }])
+  .service("orderBookService",[function(){
+    var obj = {
+      orderBooks: [],
+      totalBooks: 0
+    };
 
-angular.module("demoApp",[])
-.service("bookService",[function(){
-  var obj = {
-    books: []
-  };
+    obj.addBooks = function(newBook) {
+      for(i=0; i < obj.orderBooks.length;i++){
+        if(obj.orderBooks[i].title == newBook.title){
+          obj.orderBooks[i].orderQuantity += 1;
+          obj.totalBooks += 1;
+          return;
+        }
+      }
+        obj.orderBooks.push(newBook);
+        obj.orderBooks[i].orderQuantity += 1;
+        obj.totalBooks += 1;
+    };
 
-  obj.addBooks = function(newBook) {
-    obj.books.push(newBook);
-  };
+    obj.getBooks = function() {
+      return obj.orderBooks;
+    };
 
-  obj.getBooks = function() {
-    return obj.books;
-  };
-
-  return obj;
-}])
-.controller("BookController",[ "$scope","bookService", function($scope, bookService) {
-  $scope.books = bookService.books;
+    return obj;
+  }])
+  .controller("BookController",[ "$scope","orderBookService", function($scope, orderBookService) {
+  $scope.orderBooks = orderBookService.orderBooks;
 
   $scope.books = [
-            {title: "Awakening of intelligence", author: "Krishnamurti", price: 40},
-            {title: "AngularJS cookBook", author: "Ari Lerner", price: 30},
-            {title: "C++ Programming", author: "Bjane Stroustroup", price: 35},
-            {title: "Java Programming", author: "Daniel Liang", price: 37},
-            {title: "Ruby on Rails", author: "Michael Hartl", price: 38}
-          ];
-  $scope.addToBookList = function(book){
-    bookService.addBooks(book);
+          {title: "Awakening of intelligence", author: "Krishnamurti", price: 40, orderQuantity: 0},
+          {title: "AngularJS cookBook", author: "Ari Lerner", price: 30, orderQuantity: 0},
+          {title: "C++ Programming", author: "Bjane Stroustroup", price: 35, orderQuantity: 0},
+          {title: "Java Programming", author: "Daniel Liang", price: 37, orderQuantity: 0},
+          {title: "Ruby on Rails", author: "Michael Hartl", price: 38, orderQuantity: 0}
+        ];
+  $scope.amount = function(book){
+    return book.price * book.orderQuantity;
   };
-}]);
+  }])
+  .controller("CartController",
+    ["$scope","orderBookService",
+      function($scope,orderBookService) {
+        $scope.order = orderBookService;
+
+        $scope.addToCart = function(book){
+          orderBookService.addBooks(book);
+        };
+  }]);
 
